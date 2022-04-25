d85752f2d281fc0479463627c86e0a51c40991ef
Scott <sfitzpatrick55@gmail.com>
2022-04-25 16:49:11 -0400

    Second attempt at building InitialMigration

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Data
@@ -12,5 +13,7 @@ namespace ElevenNote.Data
         : base(options)
         {   
         }
+
+        public DbSet<UserEntity> Users { get; set; }
     }
 }
