a36546644ba157ccfdafaf6bc88dd007ff8e59cf
Scott <sfitzpatrick55@gmail.com>
2022-04-25 16:36:21 -0400

    Add line spaces to user class

--- ElevenNote.Data/Entities/UserEntity.cs
+++ ElevenNote.Data/Entities/UserEntity.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ElevenNote.Data.Entities
 {
@@ -10,17 +7,21 @@ namespace ElevenNote.Data.Entities
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
         public string Username { get; set; }
+
         [Required]
         public string Password { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        
         [Required]
         public DateTime DateCreated { get; set; }
-        
     }
 }
