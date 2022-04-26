a10c41dc45a0bdf179ff14403061d8cfbb8568c3
Scott <sfitzpatrick55@gmail.com>
2022-04-25 20:30:28 -0400

    Create RegisterUserAsync method

--- ElevenNote.Services/User/IUserService.cs
+++ ElevenNote.Services/User/IUserService.cs
@@ -1,12 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Data.Entities;
+using ElevenNote.Models.User;
 
 namespace ElevenNote.Services.User
 {
     public class IUserService
     {
-        
+        Task<bool> RegisterUserAsync(UserRegister model)
+        {
+            var entity = new UserEntity
+            {
+                Email = model.Email,
+                Username = model.Username,
+                Password = model.Password,
+                DateCreated = DateTime.Now
+            };
+        }
     }
 }
