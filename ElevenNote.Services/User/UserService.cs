a2f2a042f0b0682f342dcdfc00c2685f772e2981
Scott <sfitzpatrick55@gmail.com>
2022-04-25 20:34:42 -0400

    Create ApplicationDbContext injection

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Data;
 
 namespace ElevenNote.Services.User
 {
     public class UserService : IUserService
     {
-        
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
     }
 }
