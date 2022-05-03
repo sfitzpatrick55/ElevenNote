f4819490b50869fd9bcccd99cf9fbdb0b4dfabe1
Scott <sfitzpatrick55@gmail.com>
2022-05-02 22:26:54 -0400

    Second attempt at UserService

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -1,12 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Models.User;
+using ElevenNote.Data.Entities;
+using ElevenNote.Data;
 
 namespace ElevenNote.Services.User
 {
     public class UserService : IUserService
     {
-        
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> RegisterUserAsync(UserRegister model)
+        {
+            var entity = new UserEntity
+            {
+                Email = model.Email,
+                Username = model.Username,
+                Password = model.Password,
+                DateCreated = DateTime.Now
+            };
+        }
     }
 }
