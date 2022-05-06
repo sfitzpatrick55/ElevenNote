e859009a73b42c9ab2390fd833cb84d473c9958c
Scott <sfitzpatrick55@gmail.com>
2022-05-05 23:41:50 -0400

    Add token request and response token

---
+++ ElevenNote.Models/Token/TokenRequest.cs
@@ -0,0 +1,14 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.Models.Token
+{
+    public class TokenRequest
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ ElevenNote.Models/Token/TokenResponse.cs
@@ -0,0 +1,10 @@
+
+namespace ElevenNote.Models.Token
+{
+    public class TokenResponse
+    {
+        public string Token { get; set; }
+        public DateTime IssuedAt { get; set; }
+        public DateTime Expires { get; set; }
+    }
+}
